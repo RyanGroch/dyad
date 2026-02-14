5e277281c6ea30c8e9917591b0eb2ce8e38c3a32
Ryan Groch <ryan.w.groch@gmail.com>
2026-02-14 09:55:13 -0600

    extend all delays

--- e2e-tests/helpers/constants.ts
+++ e2e-tests/helpers/constants.ts
@@ -4,10 +4,10 @@
  */
 export const Timeout = {
   // Things generally take longer on CI, so we make them longer.
-  EXTRA_LONG: process.env.CI ? 120_000 : 60_000,
-  LONG: process.env.CI ? 60_000 : 30_000,
-  MEDIUM: process.env.CI ? 30_000 : 15_000,
-  SHORT: process.env.CI ? 5_000 : 2_000,
+  EXTRA_LONG: process.env.CI ? 240_000 : 60_000,
+  LONG: process.env.CI ? 120_000 : 30_000,
+  MEDIUM: process.env.CI ? 60_000 : 15_000,
+  SHORT: process.env.CI ? 15_000 : 2_000,
 };
 
 export const showDebugLogs = process.env.DEBUG_LOGS === "true";
